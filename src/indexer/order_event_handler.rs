882458de031beb5f407ac2404f872912b449119b
nazgull08 <nazgull08@protonmail.com>
2024-12-04 17:25:45 +0300

    added changed main asset to ETHUSDC

--- src/indexer/order_event_handler.rs
+++ src/indexer/order_event_handler.rs
@@ -1,5 +1,5 @@
 use crate::storage::candles::CandleStore;
-use log::{error, info};
+use log::error;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
@@ -33,27 +33,10 @@ pub async fn handle_order_event(
     if let Some(event_type) = event.event_type.as_deref() {
         if event_type == "Trade" {
                 if let (Some(price), Some(amount)) = (event.price, event.amount) {
-                    let asset = "AAPL"; 
-                    let genesis_block = 1; 
-                    let genesis_timestamp = 1724996333; 
-
-                    
-                    let event_time_old = genesis_timestamp + (event.block_number - genesis_block);
+                    let asset = "ETHUSDC"; 
                     let block_timestamp = event.block_timestamp;
-                    info!("================new event");
-                    info!("genesis_timestamp: {:?}", genesis_timestamp);
-                    info!("event_block_number: {:?}", event.block_number);
-                    info!("event_time: {:?}", event_time_old);
-                    info!("block_timestamp: {:?}", block_timestamp);
-                    info!("----------------new event");
                     let intervals = vec![60, 180, 300, 900, 1800, 3600, 86400, 604800, 2592000];
                     for &interval in &intervals {
-                        println!("=====================< INTERVAL {:?}", interval);
-                        println!(
-                            "Adding price to CandleStore: symbol={}, interval={}, price={}, amount={}, event_time={}",
-                            asset, interval, price, amount, block_timestamp 
-                        );
-                        println!("===================== INTERVAL {:?} >", interval);
                         candle_store.add_price(asset, interval, price as f64, amount as f64, block_timestamp);
                     }
                 } else {
