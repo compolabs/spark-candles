ddd34b0c8e7fdb788f10d8fbfa2acf291cc32d97
nazgull08 <nazgull08@protonmail.com>
2024-12-04 15:17:46 +0300

    hotfix timestamp

--- src/indexer/order_event_handler.rs
+++ src/indexer/order_event_handler.rs
@@ -8,6 +8,7 @@ pub struct PangeaOrderEvent {
     pub chain: u64,
     pub block_number: i64,
     pub block_hash: String,
+    pub block_timestamp: i64,
     pub transaction_hash: String,
     pub transaction_index: u64,
     pub log_index: u64,
@@ -37,21 +38,23 @@ pub async fn handle_order_event(
                     let genesis_timestamp = 1724996333; 
 
                     
-                    let event_time = genesis_timestamp + (event.block_number - genesis_block);
+                    let event_time_old = genesis_timestamp + (event.block_number - genesis_block);
+                    let block_timestamp = event.block_timestamp;
                     info!("================new event");
                     info!("genesis_timestamp: {:?}", genesis_timestamp);
                     info!("event_block_number: {:?}", event.block_number);
-                    info!("event_time: {:?}", event_time);
+                    info!("event_time: {:?}", event_time_old);
+                    info!("block_timestamp: {:?}", block_timestamp);
                     info!("----------------new event");
                     let intervals = vec![60, 180, 300, 900, 1800, 3600, 86400, 604800, 2592000];
                     for &interval in &intervals {
                         println!("=====================< INTERVAL {:?}", interval);
                         println!(
                             "Adding price to CandleStore: symbol={}, interval={}, price={}, amount={}, event_time={}",
-                            asset, interval, price, amount, event_time
+                            asset, interval, price, amount, block_timestamp 
                         );
                         println!("===================== INTERVAL {:?} >", interval);
-                        candle_store.add_price(asset, interval, price as f64, amount as f64, event_time);
+                        candle_store.add_price(asset, interval, price as f64, amount as f64, block_timestamp);
                     }
                 } else {
                     error!("Incomplete Trade event data: {:?}", event);
