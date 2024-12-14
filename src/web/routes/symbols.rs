ea65592b96c361f3900d7cb4c7f97832d2d239bd
nazgull08 <nazgull08@protonmail.com>
2024-12-14 16:44:06 +0300

    reworking 1

--- src/web/routes/symbols.rs
+++ src/web/routes/symbols.rs
@@ -11,28 +11,22 @@ use crate::storage::trading_engine::TradingEngine;
 #[openapi]
 #[get("/symbols?<symbol>")]
 pub async fn get_symbols(
-    symbol: Option<String>, // Параметр запроса
+    symbol: Option<String>,
     trading_engine: &State<Arc<TradingEngine>>,
 ) -> Json<serde_json::Value> {
-    let a = trading_engine.configs.keys();
-    info!("==================================");
-    info!("a {:?}",a);
-    info!("==================================");
-    // Если символ указан, ищем его в конфигурации
     if let Some(symbol) = symbol {
         if let Some(config) = trading_engine.configs.get(&symbol) {
-            // Формируем объект с полной информацией о символе
             let symbol_data = json!({
                 "symbol": config.symbol,
                 "ticker": config.symbol,
-                "name": config.description,
-                "description": config.description,
-                "type_": "crypto", // Подставь нужный тип
-                "exchange": "CryptoExchange", // Зависит от твоей логики
-                "timezone": "Etc/UTC",
+                "name": "Ethereum / USDC", // Вернуть старое значение
+                "description": "ETH to USDC", // Вернуть старое значение
+                "type_": "stock", // Вернуть старое значение
+                "exchange": "ETH USDC", // Вернуть старое значение
+                "timezone": "America/New_York", // Вернуть старую временную зону
                 "minmov": 1,
                 "pricescale": 100,
-                "session": "24x7",
+                "session": "0930-1600", // Вернуть старое значение
                 "has_intraday": true,
                 "has_daily": true,
                 "supported_resolutions": ["1", "5", "15", "30", "60", "D", "W", "M"],
@@ -41,12 +35,10 @@ pub async fn get_symbols(
             });
             return Json(symbol_data);
         } else {
-            // Если символ не найден, возвращаем ошибку
             return Json(json!({ "status": "error", "message": "Symbol not found" }));
         }
     }
 
-    // Если параметр `symbol` не указан, возвращаем все символы
     let symbols = trading_engine.get_symbols();
     Json(json!({
         "status": "ok",
